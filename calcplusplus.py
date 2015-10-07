0a7229a92d3824ddae9a62f26bfec7282bd9bbf8
cguirado <cgboluda@gmail.com>
2015-10-07 10:23:49 +0200

    OK plusplus

---
+++ calcplusplus.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import csv
+import calcoohija
+
+if __name__ == "__main__":
+
+    calcu = calcoohija.CalculadoraHija()
+    fichero = sys.argv[1]
+    with open(fichero) as csvarchivo:
+        lineas = csv.reader(csvarchivo)
+        for linea in lineas:
+            operador = linea[0]
+            diccionario = {"suma": calcu.suma, "multiplica": calcu.multiplica, "resta": calcu.resta, "divide": calcu.divide} 
+
+            if operador in ["suma", "multiplica", "resta", "divide"]:
+                calcular = linea[1]
+                for operaciones in linea[2:]:
+                    calcular = diccionario[operador](calcular, operaciones)
+                print(calcular)
