bf5ed7aa5fb7022c902c9427a18417cddf60fbd6
cguirado <cgboluda@gmail.com>
2015-10-07 10:23:30 +0200

    OK plusplus

--- calcoo.py
+++ calcoo.py
@@ -4,28 +4,35 @@
 import sys
 
 
-
 class Calculadora():
-    def suma (self, op1, op2):
+
+    def check(self, op1, op2):
+        try:
+            op1 = float(op1)
+            op2 = float(op2)
+        except ValueError:
+            sys.exit("Error: Non numerical parameters")
+        return (op1, op2)
+
+    def suma(self, op1, op2):
+        op1, op2 = self.check(op1, op2)
         return op1 + op2
-    def resta (self, op1, op2):
+
+    def resta(self, op1, op2):
+        op1, op2 = self.check(op1, op2)
         return op1 - op2
 
 if __name__ == "__main__":
-    try:
-        operando1 = float(sys.argv[1])
-        operando2 = float(sys.argv[3])
-        operacion = sys.argv[2]
-    except ValueError:
-        sys.exit("Error: Non numerical parameters")
-        
+    operando1 = sys.argv[1]
+    operando2 = sys.argv[3]
+    operacion = sys.argv[2]
+
     calcu = Calculadora()
     if operacion == "suma":
-        resultado = calcu.suma(operando1,operando2)
+        resultado = calcu.suma(operando1, operando2)
     elif operacion == "resta":
-        resultado = calcu.resta(operando1,operando2)
+        resultado = calcu.resta(operando1, operando2)
     else:
         print("Operaciones validas SUMAS O RESTAS")
 
     print (resultado)
-    
