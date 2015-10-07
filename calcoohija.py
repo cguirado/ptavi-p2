301cec9e59f4ca07bc3eef49d6916fb922396bf1
cguirado <cgboluda@gmail.com>
2015-10-07 10:23:37 +0200

    OK plusplus

--- calcoohija.py
+++ calcoohija.py
@@ -4,38 +4,35 @@
 import sys
 import calcoo
 
+
 class CalculadoraHija (calcoo.Calculadora):
     def multiplica(self, op1, op2):
+        op1, op2 = self.check(op1, op2)
         return op1 * op2
-    def divide (self, op1, op2):
-        return op1 / op2 
 
-if __name__ == "__main__":
-    try:
-        operando1 = float(sys.argv[1])
-        operando2 = float(sys.argv[3])
-        operacion = sys.argv[2]
-    except ValueError:
-        sys.exit("Error: Non numerical parameters")
-    
-    calcu = CalculadoraHija()
-
-    if operacion == "suma":
-        resultado = calcu.suma (operando1, operando2)
-    elif operacion == "resta":
-        resultado = calcu.resta (operando1, operando2)
-    elif operacion == "multiplica":
-        resultado = calcu.multiplica (operando1, operando2)
-    elif operacion == "divide":
-        if operando2 == 0:
+    def divide(self, op1, op2):
+        op1, op2 = self.check(op1, op2)
+        if op2 == 0:
             sys.exit("Division by zero is not allowed")
         else:
-            resultado = calcu.divide (operando1, operando2)
+            return op1 / op2
+
+if __name__ == "__main__":
+
+    calcu = CalculadoraHija()
+    operando1 = sys.argv[1]
+    operando2 = sys.argv[3]
+    operacion = sys.argv[2]
+
+    if operacion == "suma":
+        resultado = calcu.suma(operando1, operando2)
+    elif operacion == "resta":
+        resultado = calcu.resta(operando1, operando2)
+    elif operacion == "multiplica":
+        resultado = calcu.multiplica(operando1, operando2)
+    elif operacion == "divide":
+        resultado = calcu.divide(operando1, operando2)
     else:
         print("Operacion no valida")
 
     print(resultado)
-
-
-            
-    
